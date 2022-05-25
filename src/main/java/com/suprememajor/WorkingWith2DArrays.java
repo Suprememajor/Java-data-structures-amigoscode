d44a670acb040b079328931979016dd2d4aaf330
Suprememajor <ndipnobertetta@gmail.com>
2022-05-25 18:36:19 +0100

    Working with 2D arrays

---
+++ src/main/java/com/suprememajor/WorkingWith2DArrays.java
@@ -0,0 +1,23 @@
+package com.suprememajor;
+
+import java.util.Arrays;
+
+public class WorkingWith2DArrays {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        char[][] boardTwo = new char[][] {
+                new char[] {'-', '-', '-'},
+                new char[] {'-', '-', '-'},
+                new char[] {'-', '-', '-'}
+        };
+
+        System.out.println(Arrays.deepToString(board));
+        System.out.println(Arrays.deepToString(boardTwo));
+    }
+}

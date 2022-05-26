129717357a676bfbd127ae29f79e2000ee68ea71
Suprememajor <ndipnobertetta@gmail.com>
2022-05-26 15:15:49 +0100

    Working with Queues and Linkedlists

--- src/main/java/com/suprememajor/WorkingWithQueues.java
+++ src/main/java/com/suprememajor/WorkingWithQueues.java
@@ -1,20 +1,33 @@
 package com.suprememajor;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
-        Queue<Person> supermarket = new LinkedList<>();
-        supermarket.add(new Person("Alex", 21));
-        supermarket.add(new Person("Mariam", 18));
-        supermarket.add(new Person("Ali", 40));
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexa", 21));
+        linkedList.addFirst(new Person("Ali", 40));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext())
+            System.out.println(personListIterator.next());
+        while (personListIterator.hasPrevious())
+            System.out.println(personListIterator.previous());
+    }
 
-        System.out.println(supermarket.peek());
-        System.out.println(supermarket.size());
-        System.out.println(supermarket.poll());
-        System.out.println(supermarket.size());
-        System.out.println(supermarket.peek());
+    private static void queues() {
+        Queue<Person> queue = new LinkedList<>();
+        queue.add(new Person("Alex", 21));
+        queue.add(new Person("Mariam", 18));
+        queue.add(new Person("Ali", 40));
+
+        System.out.println(queue.peek());
+        System.out.println(queue.size());
+        System.out.println(queue.poll());
+        System.out.println(queue.size());
+        System.out.println(queue.peek());
     }
 
     static record Person(String name, int age){

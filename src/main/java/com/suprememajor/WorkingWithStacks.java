02bb87e3e52cef2d1337ee3b710aedcdc93ba8cc
Suprememajor <ndipnobertetta@gmail.com>
2022-05-26 07:22:55 +0100

    Working with Stacks

---
+++ src/main/java/com/suprememajor/WorkingWithStacks.java
@@ -0,0 +1,19 @@
+package com.suprememajor;
+
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.pop());
+        System.out.println(stack.size());
+        System.out.println(stack.empty());
+    }
+}
